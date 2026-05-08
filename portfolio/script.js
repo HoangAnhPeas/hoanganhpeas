ad9eb3d60b8b0459cff2a82f9440753c85ab1fd9
hoanganhpeas <hoanganhpeas003@gmail.com>
2026-05-08 14:31:35 +0700

    add fake database

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,25 @@
-console.log("Portfolio ready");
+async function loadProjects() {
+    const res = await fetch('projects.json');
+    const projects = await res.json();
+
+    const container = document.getElementById('project-list');
+
+    projects.forEach(project => {
+        const div = document.createElement('div');
+        div.className = 'project';
+
+        div.innerHTML = `
+            <img src="${project.image}">
+            <div>
+                <h3>${project.title}</h3>
+                <p>${project.description}</p>
+                <p class="tech">${project.tech.join(' • ')}</p>
+                <a href="${project.link}">View Project</a>
+            </div>
+        `;
+
+        container.appendChild(div);
+    });
+}
+
+loadProjects();
